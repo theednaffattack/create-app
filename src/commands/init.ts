a09ce5b19388e1a3c878636f528908080d1f56be
eddie naff <eddienaff@eddienaff.com>
2020-03-29 13:06:38 -0700

    Adjust to allow init command with no flags

---
+++ src/commands/init.ts
@@ -0,0 +1,37 @@
+import { Command, flags } from "@oclif/command";
+
+const debug = require("debug")("create-app:init");
+
+class CreateApp extends Command {
+  static description = "describe the command here";
+
+  static flags = {
+    // add --version flag to show CLI version
+    version: flags.version({ char: "v" }),
+    help: flags.help({ char: "h" }),
+    // flag with a value (-n, --name=VALUE)
+    name: flags.string({
+      char: "n",
+      description: "name to print",
+      default: "people"
+    }),
+    // flag with no value (-f, --force)
+    force: flags.boolean({ char: "f" })
+  };
+
+  static args = [{ name: "file" }];
+
+  async run() {
+    const { args, flags } = this.parse(CreateApp);
+
+    debug("parsing args", args);
+    debug("parsing flags", flags);
+    const name = flags.name || "world";
+    this.log(`hello ${name} from ./src/index.ts`);
+    if (args.file && flags.force) {
+      this.log(`you input --force and --file: ${args.file}`);
+    }
+  }
+}
+
+export = CreateApp;
